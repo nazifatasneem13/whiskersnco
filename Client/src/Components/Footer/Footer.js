0746ad8a9087bf64558217106efd0a7a2a07fdc9
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2025-01-14 16:28:01 +0600

    Update Footer.js

--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -1,36 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Box, Typography, IconButton, Container, Divider } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
 import logo from "./images/logo.jpeg";
 
 const Footer = (props) => {
   return (
-    <footer className="footer">
-      <div>
-        <Link className="logo-container" to="/">
-          <img className="navbar-logo" src={logo} alt="Whiskers&Co Logo" />
-          <p>{props.title}</p>
-        </Link>
-      </div>
-      <div className="below-footer">
-        <p>You can reach me at </p>
-        <p>
-          <a className="mail-links" href="mailto:nabilaislam21@iut-dhaka.edu">
-            nabilaislam21@iut-dhaka.edu
-          </a>
-        </p>{" "}
-        <p>
-          <a className="mail-links" href="mailto:nazifatasneem@iut-dhaka.edu">
-            nazifatasneem@iut-dhaka.edu
-          </a>
-        </p>
-        <p>
-          <a className="mail-links" href="mailto:tasniaanwer@iut-dhaka.edu">
-            tasniaanwer@iut-dhaka.edu
-          </a>
-        </p>
-        <p>&copy; 2024 whiskers&co</p>
-      </div>
-    </footer>
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "#19275c", // Midnight blue background
+        color: "#fff",
+        padding: "2rem",
+        textAlign: "center",
+      }}
+    >
+      <Container maxWidth="md">
+        {/* Logo and Title */}
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ marginBottom: "1.5rem" }}
+        >
+          <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+            <Box display="flex" alignItems="center" gap={1}>
+              <img
+                src={logo}
+                alt="Whiskers&Co Logo"
+                style={{ width: "50px", borderRadius: "50%" }}
+              />
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: "bold", fontSize: "20px" }}
+              >
+                {props.title || "Whiskers&Co"}
+              </Typography>
+            </Box>
+          </Link>
+        </Box>
+
+        <Divider sx={{ marginBottom: "1.5rem", backgroundColor: "#fff" }} />
+
+        {/* Footer Text */}
+        <Typography
+          variant="body2"
+          sx={{
+            fontSize: "0.875rem",
+            color: "#ccc",
+          }}
+        >
+          &copy; {new Date().getFullYear()} Whiskers&Co. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
