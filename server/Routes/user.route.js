126f86dd5f59bf47bc25d8275d84c3d6ef535ee6
sheona001 <sirexcc001@gmail.com>
2025-01-16 13:32:13 +0600

    unblock routes

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -5,12 +5,16 @@ const {
   savePreferences,
   updateUser,
   addToWishlist,
+  getBlockedUser,
+  unblockUser,
 } = require("../Controller/user.controller"); // Ensure this path is correct
 const { verifyToken } = require("../middleware/jwt"); // Ensure this path is correct
 
 const router = express.Router();
 
 router.delete("/:id", verifyToken, deleteUser);
+router.get("/blocked/hasblocked", verifyToken, getBlockedUser);
+router.post("/unblock", verifyToken, unblockUser);
 router.get("/:id", verifyToken, getUser);
 router.post("/preferences", verifyToken, savePreferences);
 router.get("/profile", verifyToken, getUser);
